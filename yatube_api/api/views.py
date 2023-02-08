3c952fed85f026027d3b48862313bab878c4518c
Alexander Dondokov <adondokov@gmail.com>
2023-02-09 00:36:20 +0800

    tried to restore the yesterdays code

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,10 @@
+# from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.permissions import UserOrReadOnly
-from rest_framework.permissions import AuthorOrReadOnly
-
+# from rest_framework.generics import CreateAPIView
 
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 from posts.models import Post, Group, Comment
@@ -13,7 +13,10 @@ from posts.models import Post, Group, Comment
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, UserOrReadOnly]
+    # permission_classes = [IsAuthentificated, UserOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -24,7 +27,29 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    # permission_classes = (AuthorOrReadOnly,)
+
+    # def get_queryset(self):
+    #     # our_post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+    #     # new_queryset = Comment.objects.filter(post=our_post)
+    #     # return new_queryset
+    #
+    #     new_queryset = self.queryset.filter(post_id=self.kwargs['post_id'])
+    #     return new_queryset
+
+    # def get_queryset(self):
+    #     post_id = self.kwargs.get("post_id")
+    #     new_queryset = Comment.objects.filter(post=post_id)
+    #     return new_queryset
+    #
+    # def create(self, request, post_id):
+    #     post_id = self.kwargs.get("post_id")
+    #     serializer = CommentSerializer(data=request.data)
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save(author=request.user,
+    #                         post=Post.objects.get(id=post_id))
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
     def create(self, request, post_id):
         post = Post.objects.get(id=post_id)
@@ -34,8 +59,8 @@ class CommentViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def perform_create(self, request, post_id):
-        serializer = CommentSerializer(data=request.data)
-        post = Post.objects.get(id=post_id)
-        serializer.save(author=self.request.user, post=post)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # def perform_create(self, request, post_id):
+    #     serializer = CommentSerializer(data=request.data)
+    #     post = Post.objects.get(id=post_id)
+    #     serializer.save(author=self.request.user, post=post)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
